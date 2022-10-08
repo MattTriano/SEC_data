59c06290fefb59d9c70637d857ab9c9e976d10f3
matttriano <matt.triano@gmail.com>
2022-10-08 12:55:27 -0500

    Implements quarterly archive retrievers and cleans up orchestration.

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # SEC
 
+Running data retrievers:
+
+First; create a `.env` file that contains a variable named `email` and an email address to include along with requests to the SEC. On a Linux/Unix system, you can create a properly formatted `.env` file via the command below (with the example email replaced with a real one).
+
+```bash
+user@host:~/.../sec$ echo "email=example.email@email.com" > .env
+```
+
+
+
+
 ## DERA Data Library
 
 The Division of Economic and Risk Analysis (DERA) offers investors and market participants access to aggregated data from public filings for research and analysis.
--- src/edgar_data.py
+++ src/edgar_data.py
@@ -8,6 +8,9 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.element import ResultSet
 
+def check_env_vars():
+    print(os.environ["email"])
+    return os.environ["email"]
 
 def submit_request_to_sec(url) -> requests.models.Response:
     try:
@@ -17,7 +20,7 @@ def submit_request_to_sec(url) -> requests.models.Response:
         else:
             raise Exception(
                 f"Failed to get a valid response for url '{url}'\n"
-                + f"Status code: {rest.status_code}. Is ENV VAR 'email' set?"
+                + f"Status code: {resp.status_code}. Is ENV VAR 'email' set?"
             )
     except Exception as err:
         print(f"Error: {err}; url: {url}")
@@ -54,58 +57,88 @@ def get_edgar_cik_lookup_data(data_dir: pathlib.Path, force_repull: bool = False
             )
             company_cik_codes_df.to_csv(file_path, index=False)
         else:
-            raise Exception(f"Failed to get a valid response: {rest.status_code}")
+            raise Exception(f"Failed to get a valid response: {resp.status_code}")
     return pd.read_csv(file_path, dtype=str)
 
 
-def extract_values_from_table_row(table_data_row: ResultSet, table_header: List) -> Dict:
+def extract_values_from_table_row(table_row_data: ResultSet, table_header: List) -> Dict:
     try:
         return {
-            "rel_url": table_data_row[0].find("a", href=True)["href"],
-            table_header[0]: table_data_row[0].text,
-            table_header[1]: table_data_row[1].text.replace("\xa0", " "),
-            table_header[2]: table_data_row[2].text,
+            "rel_url": table_row_data[0].find("a", href=True)["href"],
+            table_header[0]: table_row_data[0].text,
+            table_header[1]: table_row_data[1].text.replace("\xa0", " "),
+            table_header[2]: table_row_data[2].text,
         }
     except Exception as err:
-        print(f"Error: {err}, table_data_row: {table_data_row}")
+        print(f"Error: {err}, table_data_row: {table_row_data}")
 
 
 def extract_year_table_rows_from_edgar_archive_feed() -> ResultSet:
     base_url = "https://www.sec.gov/Archives/edgar/Feed/"
-    resp = requests.get(base_url, headers={"User-Agent": os.environ["email"]})
-    if resp.status_code == 200:
-        soup = BeautifulSoup(resp.content)
-        tables = soup.find_all("table")
-        assert (
-            len(tables) == 1
-        ), f"More tables than expected found on the page at {base_url}. Investigate."
-        year_table = tables[0]
-        year_table_rows = year_table.find_all("tr")
-        return year_table_rows
-    else:
-        raise Exception(
-            f"Failed to get a valid response: {rest.status_code}. Is ENV VAR 'email' set?"
-        )
+    resp = submit_request_to_sec(url=base_url)
+    year_table_rows = extract_table_rows_from_response(resp=resp)
+    return year_table_rows
 
 
-def extract_data_from_edgar_table_rows(year_table_rows: ResultSet) -> pd.DataFrame:
-    table_header_line = year_table_rows[0].find_all("th")
+def extract_year_qtr_table_rows_from_edgar_archive_feed(year_url: str) -> ResultSet:
+    resp = submit_request_to_sec(url=year_url)
+    qtr_table_rows = extract_table_rows_from_response(resp=resp)
+    return qtr_table_rows
+
+
+def extract_data_from_edgar_table_rows(table_rows: ResultSet) -> pd.DataFrame:
+    table_header_line = table_rows[0].find_all("th")
     table_header = [el.text for el in table_header_line]
     data_rows = []
-    for year_table_row in year_table_rows[1:]:
-        table_data_row = year_table_row.find_all("td")
+    for table_row in table_rows[1:]:
+        table_row_data = table_row.find_all("td")
         data_rows.append(
-            extract_values_from_table_row(table_data_row=table_data_row, table_header=table_header)
+            extract_values_from_table_row(table_row_data=table_row_data, table_header=table_header)
         )
-    year_page_df = pd.DataFrame(data_rows)
-    return year_page_df
+    page_df = pd.DataFrame(data_rows)
+    return page_df
 
 
 def get_egdar_archive_years(data_dir: pathlib.Path, check_page: bool = False) -> pd.DataFrame:
     file_path = data_dir.joinpath("edgar_archive_years.csv")
     if not file_path.is_file() or check_page:
         year_table_rows = extract_year_table_rows_from_edgar_archive_feed()
-        edgar_archive_years_df = extract_data_from_edgar_table_rows(year_table_rows=year_table_rows)
+        edgar_archive_years_df = extract_data_from_edgar_table_rows(table_rows=year_table_rows)
+        edgar_archive_years_df["year_url"] = (
+            "https://www.sec.gov/Archives/edgar/Feed/" + edgar_archive_years_df["rel_url"]
+        )
+        edgar_archive_years_df["Last Modified"] = pd.to_datetime(
+            edgar_archive_years_df["Last Modified"]
+        )
         edgar_archive_years_df.to_csv(file_path, index=False)
     edgar_archive_years_df = pd.read_csv(file_path, dtype=str)
     return edgar_archive_years_df
+
+
+def extract_table_rows_from_response(
+    resp: requests.models.Response, tables_expected: int = 1
+) -> List:
+    soup = BeautifulSoup(resp.content, "html.parser")
+    tables = soup.find_all("table")
+    assert (
+        len(tables) == tables_expected
+    ), f"More tables than expected found on the page at {resp.url}. Investigate."
+    table_rows = tables[0].find_all("tr")
+    return table_rows
+
+
+def get_edgar_archive_year_quarters(
+    year: str, data_dir: pathlib.Path, check_page: bool = False
+) -> pd.DataFrame:
+    archive_year_dir = data_dir.joinpath("archive", year)
+    archive_year_dir.mkdir(exist_ok=True, parents=True)
+    file_path = archive_year_dir.joinpath(f"edgar_qtr_data_for_{year}.csv")
+    if not file_path.is_file() or check_page:
+        year_url = f"https://www.sec.gov/Archives/edgar/Feed/{year}"
+        resp = submit_request_to_sec(url=year_url)
+        qtr_table_rows = extract_table_rows_from_response(resp=resp)
+        edgar_archive_year_qtrs_df = extract_data_from_edgar_table_rows(table_rows=qtr_table_rows)
+        edgar_archive_year_qtrs_df["year"] = year
+        edgar_archive_year_qtrs_df.to_csv(file_path, index=False)
+    edgar_archive_year_qtrs_df = pd.read_csv(file_path, dtype=str)
+    return edgar_archive_year_qtrs_df
